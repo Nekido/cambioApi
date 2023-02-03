eefb8e98ae2ae2c0d8f365788e4bc10366a0add2
Caterine Studzinski <89097644+Nekido@users.noreply.github.com>
2023-02-03 20:21:36 -0300

    Initial commit

---
+++ ordemCompra/src/main/java/br/com/cambio/ordemCompra/OrdemCompraApplication.java
@@ -0,0 +1,13 @@
+package br.com.cambio.ordemCompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemCompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemCompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cambio/cambioApi/CambioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cambio.cambioApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
