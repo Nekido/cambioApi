cfdd9d4470e8fa5a08bf587990afb9722455587e
flaviaadiniz <flaviaadiniz@gmail.com>
2023-02-07 18:40:12 -0300

    Adição dos parâmetros que faltavam na classe OrdemCompra e adição da classe OrdemCompraDTO

--- ordemCompra/src/main/java/br/com/cambio/ordemCompra/OrdemCompra.java
+++ ordemCompra/src/main/java/br/com/cambio/ordemCompra/OrdemCompra.java
@@ -6,15 +6,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+import java.time.LocalDate;
 
 @Entity
 @Data
 public class OrdemCompra {
     @Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    private Long idCliente;
     private String cpfCliente;
+    private LocalDate dataSolicitacao;
     private TipoMoeda tipoMoedaEstrangeira;
     private Double valorMoedaEstrangeira;
+    private Double valorCotacao;
+    private Double valorTotalOperacao;
     private String numeroAgenciaDeRetirada;
 }
---
+++ ordemCompra/src/main/java/br/com/cambio/ordemCompra/OrdemCompraDTO.java
@@ -0,0 +1,46 @@
+package br.com.cambio.ordemCompra;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class OrdemCompraDTO {
+
+    private Long Id;
+    private Long idCliente;
+    private String cpfCliente;
+    private LocalDate dataSolicitacao;
+    private TipoMoeda tipoMoedaEstrangeira;
+    private Double valorMoedaEstrangeira;
+    private Double valorCotacao;
+    private Double valorTotalOperacao;
+    private String numeroAgenciaDeRetirada;
+
+    public static OrdemCompraDTO of(OrdemCompra ordemCompra) {
+        OrdemCompraDTO dto = new OrdemCompraDTO();
+        dto.setId(ordemCompra.getId());
+        dto.setIdCliente(ordemCompra.getIdCliente());
+        dto.setCpfCliente(ordemCompra.getCpfCliente());
+        dto.setDataSolicitacao(ordemCompra.getDataSolicitacao());
+        dto.setTipoMoedaEstrangeira(ordemCompra.getTipoMoedaEstrangeira());
+        dto.setValorCotacao(ordemCompra.getValorCotacao());
+        dto.setValorTotalOperacao(ordemCompra.getValorTotalOperacao());
+        dto.setNumeroAgenciaDeRetirada(ordemCompra.getNumeroAgenciaDeRetirada());
+        return dto;
+    }
+
+    public OrdemCompra toEntity (){
+        OrdemCompra entity = new OrdemCompra();
+        entity.setId(Id);
+        entity.setIdCliente(idCliente);
+        entity.setCpfCliente(cpfCliente);
+        entity.setDataSolicitacao(dataSolicitacao);
+        entity.setTipoMoedaEstrangeira(tipoMoedaEstrangeira);
+        entity.setValorCotacao(valorCotacao);
+        entity.setValorTotalOperacao(valorTotalOperacao);
+        entity.setNumeroAgenciaDeRetirada(numeroAgenciaDeRetirada);
+        return entity;
+    }
+
+}
